921b59d864b1fdd52519f8de4f13a3787b2d64aa
felipe.piacsek <felipe.piacsek@gmail.com>
2021-10-24 22:39:22 -0300

    [TEST] Ensures users API returns the expected serialized data

--- test/controllers/v1/users_controller_test.rb
+++ test/controllers/v1/users_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
+require_relative '../../../app/serializers/v1/user_serializer'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index with expected serialization" do
+  test "should get index with expected serialization - many users" do
+    expected_response = V1::UserSerializer.new.serialize_many(User.all).to_json
+
     get v1_users_url
+
     assert_response :success
+    assert expected_response == response.body
   end
 end
